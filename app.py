0ebc42df5002bc624476122ee5c28f1d1572acba
Eagle Lin <daolook@gmail.com>
2019-06-01 15:08:46 +0800

    add second line text message

--- app.py
+++ app.py
@@ -39,10 +39,8 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        StickerSendMessage(
-            package_id='1',
-            sticker_id='1'
-        )
+        TextSendMessage(text='您說的是: '+ event.message.text)
+        TextSendMessage(text='這是我的名片: '+ event.message.text)
     )
 
 

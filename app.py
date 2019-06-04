4a162aad605484322dee4d43eaaf1d65b38847e3
Eagle Lin <daolook@gmail.com>
2019-06-05 02:29:43 +0800

    fix eror

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def handle_message(event):
 
         line_bot_api.reply_message(
             event.reply_token,
-            confirm_template
+            Confirm_template
         )
 
     else:
